122fb326233e59a2cd0fbb001672ca50892c442e
Kseniya Ivanova <64292131+ksuivanova@users.noreply.github.com>
2020-05-14 15:06:06 +0200

    Add files via upload

---
+++ ispalyndrom.py
@@ -0,0 +1,18 @@
+
+def IsPalyndrom(s):
+    n = len(s)
+    i=0
+    j=n-1
+    while j > i  :
+        while i < n and s[i].isalpha()==False :
+            i+=1
+        while j > 0 and s[j].isalpha()==False:
+            j-=1
+        if j < i:
+            return True
+        if s[i].lower()==s[j].lower():
+            i+=1
+            j-=1 
+        else:
+            return False               
+    return True
