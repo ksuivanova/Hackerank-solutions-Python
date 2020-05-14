1be022f1162892c00f1efe64c6a0ece0b176e879
Kseniya Ivanova <64292131+ksuivanova@users.noreply.github.com>
2020-05-15 01:10:26 +0200

    Add files via upload

--- ispalyndrom.py
+++ ispalyndrom.py
@@ -1,6 +1,6 @@
-
-def IsPalyndrom(s):
+def IsPalyndrom1(s):
     n = len(s)
+    print(n)
     i=0
     j=n-1
     while j > i  :
@@ -16,3 +16,21 @@ def IsPalyndrom(s):
         else:
             return False               
     return True
+
+def IsPalyndrom2(s):
+    s1="".join([ch for ch in s.lower() if ch.isalpha()])
+    return s1==s1[::-1]
+
+def IsPalyndrom3(s):
+    s=[ch for ch in s.lower() if ch.isalpha()]
+    for i in range(len(s)//2):
+        if s[i]!=s[-i-1]:
+            return False
+    return True 
+
+
+#main
+if __name__ == '__main__':
+
+    s = "No lemon, no melon"
+    print(IsPalyndrom3(',;:  '))
